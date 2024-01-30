b651045786f095438f194562fdb6087a628a09c0
Vladyslav Teteruk <teteryk.vladislav@gmail.com>
2024-01-31 00:19:43 +0200

    Add REST API implementation

---
+++ controllers/contactsControllers.js
@@ -0,0 +1,51 @@
+const contactsService = require("../services/contactsServices.js");
+const HttpError = require("../helpers/HttpError");
+const controllerWrapper = require("../helpers/controllerWrapper.js");
+
+const getAllContacts = async (req, res) => {
+  const result = await contactsService.listContacts();
+  res.json(result);
+};
+
+const getOneContact = async (req, res) => {
+  const { id } = req.params;
+  const result = await contactsService.getContactById(id);
+  if (!result) {
+    throw HttpError(404);
+  }
+  res.json(result);
+};
+
+const deleteContact = async (req, res) => {
+  const { id } = req.params;
+  const result = await contactsService.removeContact(id);
+  if (!result) {
+    throw HttpError(404);
+  }
+  res.json(result);
+};
+
+const createContact = async (req, res) => {
+  const { name, email, phone } = req.body;
+  const result = await contactsService.addContact(name, email, phone);
+  res.status(201).json(result);
+};
+
+const updateContact = async (req, res) => {
+  const { id } = req.params;
+  const updatedItems = req.body;
+
+  const result = await contactsService.updateById(id, updatedItems);
+  if (!result) {
+    throw HttpError(404);
+  }
+  res.status(200).json(result);
+};
+
+module.exports = {
+  getAllContacts: controllerWrapper(getAllContacts),
+  getOneContact: controllerWrapper(getOneContact),
+  createContact: controllerWrapper(createContact),
+  deleteContact: controllerWrapper(deleteContact),
+  updateContact: controllerWrapper(updateContact),
+};
---
+++ routes/contactsRouter.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const {
+  getAllContacts,
+  getOneContact,
+  deleteContact,
+  createContact,
+  updateContact,
+} = require("../controllers/contactsControllers.js");
+
+const validateBody = require("../helpers/validateBody.js");
+const {
+  createContactSchema,
+  updateContactSchema,
+} = require("../schemas/contactsSchemas.js");
+
+const contactsRouter = express.Router();
+
+contactsRouter.get("/", getAllContacts);
+
+contactsRouter.get("/:id", getOneContact);
+
+contactsRouter.delete("/:id", deleteContact);
+
+contactsRouter.post("/", validateBody(createContactSchema), createContact);
+
+contactsRouter.put("/:id", validateBody(updateContactSchema), updateContact);
+
+module.exports = contactsRouter;
