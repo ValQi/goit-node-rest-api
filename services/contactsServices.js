1dd54a4f31bdbb33ea8f8d3d5c55d585b04ca1af
Vladyslav Teteruk <teteryk.vladislav@gmail.com>
2024-01-31 00:32:13 +0200

    sss

--- services/contactsServices.js
+++ services/contactsServices.js
@@ -2,10 +2,10 @@ const fs = require("fs/promises");
 const path = require("path");
 const { nanoid } = require("nanoid");
 
-const contactsPath = path.join(__dirname, "..", "db", "contacts.json"); //пас джоін обєднання шляху до поточного каталогу, папки і імелі файлу
+const contactsPath = path.join(__dirname, "..", "db", "contacts.json");
 
 async function listContacts() {
-  const data = await fs.readFile(contactsPath, "utf-8"); //parse сам робе ютф-8, можем не прописувати
+  const data = await fs.readFile(contactsPath, "utf-8"); 
   return JSON.parse(data);
 }
 
@@ -17,9 +17,9 @@ async function getContactById(id) {
 
 async function removeContact(contactId) {
   const contacts = await listContacts();
-  const index = contacts.findIndex((item) => item.id === contactId); //пошук книжки яку треба видалити
-  if (index === -1) return null; //якщо книжку не знайшли повернули нал
-  const [result] = contacts.splice(index, 1); //повернувся масив з одним елементов видаленим, і записали в змінну
+  const index = contacts.findIndex((item) => item.id === contactId);
+  if (index === -1) return null; 
+  const [result] = contacts.splice(index, 1); 
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
   return result;
 }
@@ -40,13 +40,12 @@ async function addContact(name, email, phone) {
 async function updateById(id, updatedItems) {
   const contacts = await listContacts();
 
-  const index = contacts.findIndex((item) => item.id === id); //знаходимо індекс контакта по айді який треба оновити
+  const index = contacts.findIndex((item) => item.id === id); 
   if (index === -1) return null;
 
-  const oldContact = contacts[index]; //отримуєму існуючий контакт за індексом
-  const updatedContact = { ...oldContact, ...updatedItems }; //створюємо оновленій контакт, додаючи оновленні дані до існуючих
-  contacts[index] = updatedContact; //заміняємо контакт на оновлений
-
+  const oldContact = contacts[index]; 
+  const updatedContact = { ...oldContact, ...updatedItems }; 
+  contacts[index] = updatedContact; 
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
   return updatedContact;
 }
